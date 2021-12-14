93c62cc984387fe1f383b55bb9330b8de353f676
Areg Vardanian <aregv@bk.ru>
2021-12-14 20:07:29 +0300

    title show

--- CryptoTrack/ViewController.swift
+++ CryptoTrack/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 // UI to show different cryptos
 // MVVM
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -22,9 +23,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Crypto Tracker"
         view.addSubview(tableView)
         tableView.dataSource = self
-        
+        tableView.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CryptoTableViewCell.identifier,
+            for: indexPath
+        ) as? CryptoTableViewCell else {
+            fatalError()
+        }
+        cell.textLabel?.text = "Hello World"
+        return cell
     }
 }
 
