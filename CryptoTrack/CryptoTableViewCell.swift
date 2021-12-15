785459d6313584f5a8c08d8f2044b4f874b644c2
Areg Vardanian <aregv@bk.ru>
2021-12-15 17:41:33 +0300

    finish configure run

--- CryptoTrack/CryptoTableViewCell.swift
+++ CryptoTrack/CryptoTableViewCell.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-struct CryptoTableViewCellViewModel {
+class CryptoTableViewCellViewModel {
     let name: String
     let symbol: String
     let price: String
     let iconUrl: URL?
+    var iconData: Data?
+    
+    init(
+        name: String,
+        symbol: String,
+        price: String,
+        iconUrl: URL?
+    ) {
+        self.name = name
+        self.symbol = symbol
+        self.price = price
+        self.iconUrl = iconUrl
+    }
 }
 
 class CryptoTableViewCell: UITableViewCell {
@@ -101,15 +114,20 @@ class CryptoTableViewCell: UITableViewCell {
         priceLabel.text = viewModel.price
         symbolLabel.text = viewModel.symbol
         
-        if let url = viewModel.iconUrl {
-            let task = URLSession.shared.dataTask(with: url) { [ weak self ] data, _, _ in
-                if let data = data {
-                    DispatchQueue.main.async {
-                        self?.iconImageView.image = UIImage(data: data)
+        if let data = viewModel.iconData {
+            iconImageView.image = UIImage(data: data)
+        } else {
+            if let url = viewModel.iconUrl {
+                let task = URLSession.shared.dataTask(with: url) { [ weak self ] data, _, _ in
+                    if let data = data {
+                        viewModel.iconData = data
+                        DispatchQueue.main.async {
+                            self?.iconImageView.image = UIImage(data: data)
+                        }
                     }
                 }
+                task.resume()
             }
-            task.resume()
         }
     }
     
